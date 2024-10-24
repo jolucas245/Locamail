38ffa4f3eed81484fc3d3a323021f154f3e04e20
gustavo.jesus <fernandes.man@outlook.com>
2024-10-24 12:58:16 -0300

    Inclusão da estrutura do send e-mail

--- app/src/main/java/br/com/fiap/locamail/views/send/SendView.kt
+++ app/src/main/java/br/com/fiap/locamail/views/send/SendView.kt
@@ -1,8 +1,106 @@
 package br.com.fiap.locamail.views.send
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SendView() {
 
+
 }
+
+@Preview(showBackground = true)
+@Composable
+fun SendViewPreview() {
+
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        Text(
+            text = "Topbar",
+            Modifier.fillMaxWidth(),
+            fontSize = 28.sp,
+        )
+        var destination by remember {
+            mutableStateOf("")
+        }
+        TextField(
+            value = destination,
+            onValueChange = {
+                destination = it
+            },
+            Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Para:")
+            },
+        )
+        var origin by remember {
+            mutableStateOf("")
+        }
+        TextField(
+            value = origin,
+            onValueChange = {
+                origin = it
+            },
+            Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "De:")
+            },
+        )
+        var subject by remember {
+            mutableStateOf("")
+        }
+        TextField(
+            value = subject,
+            onValueChange = {
+                subject = it
+            },
+            Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Assunto:")
+            },
+        )
+        var description by remember {
+            mutableStateOf("")
+        }
+        TextField(
+            value = description,
+            onValueChange = {
+                description = it
+            },
+            Modifier
+                .fillMaxWidth()
+                .heightIn(min = 100.dp),
+            label = {
+                Text(text = "Escrever e-mail...")
+            },
+        )
+        Button(
+            onClick = { /*TODO*/ },
+            Modifier.fillMaxWidth(),
+        ) {
+            Text(text = "Enviar")
+        }
+    }
+}
+
+
